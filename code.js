fb53b33b27d0de7070df8f8c1dba3bee3607adfb
MarionDesmonnet <marion.desmonnet@gmail.com>
2016-06-19 21:04:51 +0200

    FUN

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'hello *tag': function(tag) {
-      alert(tag);
-    }
+    'je dis *tag': function(tag) {
+      
+    var nouveauDiv = document.createElement("div");
+    nouveauDiv.innerHTML = "<h1>" + tag + "</h1>";
+  
+    var mon_div = document.getElementById("org_div1");
+    document.body.insertBefore(nouveauDiv, mon_div);
+      }
   };
   
   annyang.debug();
