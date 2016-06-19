f3c9b7b7b05fd39ce5bad40763c7d8e265e3a24f
Marion Desmonnet <marion.desmonnet@gmail.com>
2016-06-19 20:51:05 +0200

    Initial commit

---
+++ code.js
@@ -0,0 +1,17 @@
+if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'hello *tag': function(tag) {
+      alert(tag);
+    }
+  };
+  
+  annyang.debug();
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+  
+}
